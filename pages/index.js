e46c528917695e8571edf1f303d07e2ab0a60de0
Grace Gabrielle <ggabrielle@myseneca.ca>
2026-04-12 23:56:31 -0400

    fix: add jwt-decode dependency

--- pages/index.js
+++ pages/index.js
@@ -6,9 +6,9 @@
 * 
 * https://www.senecapolytechnic.ca/about/policies/academic-integrity-policy.html
 * 
-* Name: Grace Gabrielle    Student ID: __________    Date: April 12, 2026
+* Name: Grace Gabrielle    Student ID: 114693245    Date: April 12, 2026
 *
-* Vercel App (Deployed) Link: _____________________________________________________
+* Vercel App (Deployed) Link: https://web422-a3-user-api-henna.vercel.app
 *
 ********************************************************************************/
 import SearchForm from "@/components/SearchForm";
